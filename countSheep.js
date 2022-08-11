1790763dc8a5a8ff18ae45661f3447065a624b03
BrandotheCluff <brandon.cluff2@gmail.com>
2022-08-10 20:11:03 -0600

    counting sheep

---
+++ countSheep.js
@@ -0,0 +1,14 @@
+var countSheep = function (num){
+    let str = "";
+     for(let i = 1; i <= num; i++) { str+= `${i} sheep...`; }
+     return str;
+   }
+   
+   //First, set a variable as an empty string.
+   
+   //Second, write a for loop with num as the other variable.
+   
+   //Third, after each revolution of the loop have the string add 1(+=) to the string with backticks
+   //{i} sheep...
+   
+   //Fourth, return string variable
